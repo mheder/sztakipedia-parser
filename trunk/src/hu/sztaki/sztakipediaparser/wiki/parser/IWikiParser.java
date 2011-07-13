83efc253641cf700987fe3bb6254c4cdd31b2357
illes.solt@gmail.com <illes.solt@gmail.com@852ca615-f5d7-3d02-2334-ff93ade4f980>
2011-07-13 14:23:38 +0000

    removed old (non-JavaCC) codebase

--- trunk/src/hu/sztaki/sztakipediaparser/wiki/parser/IWikiParser.java
+++
@@ -1,7 +0,0 @@
-package hu.sztaki.sztakipediaparser.wiki.parser;
-
-import hu.sztaki.sztakipediaparser.wiki.converter.IWikiConverter;
-
-public interface IWikiParser {
-	public String parse(IWikiConverter c) throws ParseException, TokenMgrError;
-}
